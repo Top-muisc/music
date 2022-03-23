ace335a10a28ffa49e021779ac077b3d208da1c9
Top-muisc <102245198+Top-muisc@users.noreply.github.com>
2022-03-24 00:06:40 +0300

    Update admins.py

--- Musictop/helpers/admins.py
+++ Musictop/helpers/admins.py
@@ -2,8 +2,8 @@ from typing import List
 
 from pyrogram.types import Chat
 
-from Musicjmthon.helpers.get_admins import get as gett
-from Musicjmthon.helpers.get_admins import set
+from Musictop.helpers.get_admins import get as gett
+from Musictop.helpers.get_admins import set
 
 
 async def get_administrators(chat: Chat) -> List[int]:
